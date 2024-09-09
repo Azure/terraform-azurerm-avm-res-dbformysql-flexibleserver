b8433fc8738803e2c3dc2f2a20e2fa7f4e682930
mbilalamjad <79409563+mbilalamjad@users.noreply.github.com>
2024-09-08 22:49:55 -0700

    update

--- examples/server-config/main.tf
+++ examples/server-config/main.tf
@@ -17,7 +17,7 @@ provider "azurerm" {
 }
 
 ## Section to provide a random Azure region for the resource group
-# This allows us to randomize the region for the resource group.
+# This allows us to randomize the  region for the resource group.
 module "regions" {
   source  = "Azure/regions/azurerm"
   version = ">= 0.3.0"
