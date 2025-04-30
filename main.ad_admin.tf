9107aabf6edeb17ae3148fc2e4514b303f508aee
mbilalamjad <79409563+mbilalamjad@users.noreply.github.com>
2025-04-30 00:33:37 -0700

    Update main.ad_admin.tf

--- main.ad_admin.tf
+++ main.ad_admin.tf
@@ -2,7 +2,7 @@ resource "azurerm_mysql_flexible_server_active_directory_administrator" "this" {
   # only create the resource if the user has supplied parameters to var.active_directory_administrator.
   count = var.active_directory_administrator != null ? 1 : 0
 
-  identity_id = var.active_directory_administrator.identity_id
+  identity_id = coalesce(try(var.active_directory_administrator.identity_id, null), azurerm_mysql_flexible_server.this.identity[0].identity_ids)
   login       = var.active_directory_administrator.login
   object_id   = var.active_directory_administrator.object_id
   server_id   = azurerm_mysql_flexible_server.this.id
