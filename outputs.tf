31838f2c88fa2352b8b2c561dd6fe5cf1d334579
lonegunman <lonegunmanb@hotmail.com>
2026-03-20 03:13:51 +0800

    chore: pre-commit sync - reorder outputs alphabetically

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
-output "resource_id" {
-  description = "The ID of the resoure"
-  value       = azurerm_mysql_flexible_server.this.id
-}
-
 output "resource_fqdn" {
   description = "The fully qualified domain name of the MySQL Flexible Server."
   value       = azurerm_mysql_flexible_server.this.fqdn
+}
+
+output "resource_id" {
+  description = "The ID of the resoure"
+  value       = azurerm_mysql_flexible_server.this.id
 }
 
 output "resource_name" {
